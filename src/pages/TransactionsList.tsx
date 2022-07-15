cf9af65261b45da0cc8dac58d01cb8e3ad2f2f70
Jacob <jacobjames1117@gmail.com>
2022-07-15 10:47:04 -0600

    transactions list improved

--- src/pages/TransactionsList.tsx
+++ src/pages/TransactionsList.tsx
@@ -1,7 +1,47 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Expenses from "../components/Expenses/Expenses";
+
+
+
 
 const TransactionsList =()=>{
+
+
+    const expenses = [
+        {
+          id: "e1",
+          title: "Toilet Paper",
+          amount: 94.12,
+          date: new Date(2020, 7, 14),
+        },
+        { id: "e2", 
+          title: "New TV", 
+          amount: 799.49, 
+          date: new Date(2021, 2, 12) 
+        },
+        {
+          id: "e3",
+          title: "Car Insurance",
+          amount: 294.67,
+          date: new Date(2021, 2, 28),
+        },
+        {
+          id: "e4",
+          title: "New Desk (Wooden)",
+          amount: 450,
+          date: new Date(2021, 5, 12),
+        },
+
+        {
+            id: "e4",
+            title: "New Desk (Wooden)",
+            amount: 450,
+            date: new Date(2021, 5, 12),
+          },
+      ];
+
+console.log("Length = ",expenses[0].title);
 
     const navigate = useNavigate();
 
@@ -9,7 +49,10 @@ const TransactionsList =()=>{
         navigate("/transactions/details")
     }
     return (
-        <div onClick={transactionHandler}>Transactions List</div>
+        <div>
+            <div onClick={transactionHandler}>Transactions List</div>
+            <Expenses items = {expenses}/>
+        </div>
     );
 };
 
